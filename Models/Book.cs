9fca08d0fc53a5d4b01014de2f2ecbfcb48c1935
admin <admin@DESKTOP-M1PMCQ8>
2022-10-14 16:51:13 +0700

    update

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,22 +8,29 @@ namespace FPTBook.Models
     public class Book
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "This field cannot be empty")]
         [MinLength(3, ErrorMessage = "Title has to be more than 3 chars")]
         public string Title { get; set; }
+
         [Required(ErrorMessage = "This field cannot be empty")]
         [MinLength(3, ErrorMessage = "Author's name has to be more than 3 chars")]
         public string Author { get; set; }
+
         [MinLength(3, ErrorMessage = "Publisher's name has to be more than 3 chars")]
         public string Publisher { get; set; }
+
         [Required(ErrorMessage = "This field cannot be empty")]
         [DateValidation(ErrorMessage = "Year is invalid")]
         public int Year { get; set; }
+
         [Range(1,999999,ErrorMessage = "Max is 99999, min is 1")]
         public int Page { get; set; }
+
         [Required(ErrorMessage = "This field cannot be empty")]
         [Range(1, 999999, ErrorMessage = "Max is 999999, min is 1")]
         public double Price { get; set; }
+
         [Required(ErrorMessage = "This field cannot be empty")]
         [Range(1, 999, ErrorMessage = "Max is 999, min is 1")]
         public int Quantity { get; set; }
