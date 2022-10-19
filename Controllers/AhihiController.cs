d621fe486b71a3946923b7e1837da36d044aa9e7
phamducphutrong <phamducphutrong@gmail.com>
2022-10-19 13:54:40 +0700

    add file rác

---
+++ Controllers/AhihiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBook.Controllers
+{
+    public class AhihiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
