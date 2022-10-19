cf96fe6d7b242e39e5228ec757535f35aba09a1d
phamducphutrong <phamducphutrong@gmail.com>
2022-10-19 14:42:35 +0700

    Uploading code to GitHub by Trọng Gaming :))

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using FPTBook.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -16,11 +17,97 @@ namespace FPTBook.Data
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             SeedCategory(builder);
             SeedBook(builder);
+            SeedUser(builder);
+            SeedRole(builder);
+            SeedUserRole(builder);
+        }
+
+        private void SeedUser(ModelBuilder builder)
+        {
+            var admin = new IdentityUser
+            {
+                Id = "1",
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                NormalizedUserName = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var customer = new IdentityUser
+            {
+                Id = "2",
+                UserName = "customer@gmail.com",
+                Email = "customer@gmail.com",
+                NormalizedUserName = "customer@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var storeOwner = new IdentityUser
+            {
+                Id = "3",
+                UserName = "storeOwner@gmail.com",
+                Email = "storeOwner@gmail.com",
+                NormalizedUserName = "storeOwner@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            admin.PasswordHash = hasher.HashPassword(admin, "123456");
+            customer.PasswordHash = hasher.HashPassword(customer, "123456");
+            storeOwner.PasswordHash = hasher.HashPassword(storeOwner, "123456");
+            builder.Entity<IdentityUser>().HasData(admin, customer, storeOwner);
+        }
+
+        private void SeedRole(ModelBuilder builder)
+        {
+            var admin = new IdentityRole
+            {
+                Id = "admin",
+                Name = "Administrator",
+                NormalizedName = "Administrator"
+            };
+            var customer = new IdentityRole
+            {
+                Id = "customer",
+                Name = "Customer",
+                NormalizedName = "Customer"
+            };
+            var storeOwner = new IdentityRole
+            {
+                Id = "storeOwner",
+                Name = "StoreOwner",
+                NormalizedName = "StoreOwner"
+            };
+
+            builder.Entity<IdentityRole>().HasData(admin, customer, storeOwner);
+        }
+
+        private void SeedUserRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "1",
+                    RoleId = "admin"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "2",
+                    RoleId = "customer"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "3",
+                    RoleId = "storeOwner"
+                }
+             );
         }
 
         private void SeedBook(ModelBuilder builder)
